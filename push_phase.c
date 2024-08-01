1736f2f320406008630b72d0dde08baa0b98818b
607mark <40577872+607mark@users.noreply.github.com>
2024-08-02 00:48:32 +0300

    30k moves lol

--- push_phase.c
+++ push_phase.c
@@ -136,6 +136,108 @@ void	init_best_node(t_best_node *best)
 	best->cost = INT_MAX;
 }
 
+void apply_move(t_stacks *s, t_pair_cost *c, t_best_node *b, int n)
+{
+	int target;	
+	
+	target = target_b(s->a, s->b, s->len_a, s->len_b, b->i);
+//	printf("target%d\n", target);
+//	printf("str %d\n", n);
+
+	if (n == 1)
+	{
+		while(c->a_top)
+		{
+			op_ra(s->a, s->len_a);
+			c->a_top--;
+		}
+		while(c->b_bot)
+		{
+			op_rrb(s->b, s->len_b);
+			c->b_bot--;
+		}
+	}
+	else if (n == 2)
+	{
+		while(c->b_top)
+		{
+			op_rb(s->b, s->len_b);
+			c->b_top--;
+		}	
+		while(c->a_bot)
+		{
+			op_rra(s->a, s->len_a);
+			c->a_bot--;
+		}
+	}
+	else if (n == 3)
+	{
+		while(c->b_top && c->a_top)
+		{
+			op_rr(s->a, s->b, s->len_a, s->len_b);
+			c->b_top--;
+			c->a_top--;
+		}
+		while(c->a_top)
+		{
+			op_ra(s->a, s->len_a);
+			c->a_top--;
+		}
+	}
+	else if (n == 4)
+	{
+		while(c->b_top && c->a_top)
+		{
+			op_rr(s->a, s->b, s->len_a, s->len_b);
+			c->b_top--;
+			c->a_top--;
+		}
+		while(c->b_top)
+		{
+			op_rb(s->b, s->len_b);
+			c->b_top--;
+		}
+	}
+	else if (n == 5)
+	{
+		while(c->b_bot && c->a_bot)
+		{
+			op_rrr(s->a, s->b, s->len_a, s->len_b);
+			c->b_bot--;
+			c->a_bot--;
+		}
+		while(c->a_bot)
+		{
+			op_rra(s->b, s->len_b);
+			c->a_bot--;
+		}
+	}
+	else if (n == 6)
+	{
+		while(c->b_bot && c->a_bot)
+		{
+			op_rrr(s->a, s->b, s->len_a, s->len_b);
+			c->b_bot--;
+			c->a_bot--;
+		}
+		while(c->b_bot)
+		{	
+			op_rrb(s->b, s->len_b);
+			c->b_bot--;
+		}
+		
+	}
+	op_pb(s->a, s->b, s->len_a, s->len_b);
+	
+
+
+
+
+
+
+
+	return ;
+}
 
 void	push_phase(int *s_a, int *s_b, int *len_a, int *len_b)
 {
@@ -147,18 +249,16 @@ void	push_phase(int *s_a, int *s_b, int *len_a, int *len_b)
 	t_best_node best;
 	init_struct(&stacks, s_a, s_b, len_a, len_b);
 	init_best_node (&best);
-	//i = 2;
-	i = 249;
+	i = 2;
+
 	while (*len_a > 3 && i--)
 		op_pb(s_a, s_b, len_a, len_b);
-	//while (*len_a > 3)
-	//{
-		print_stacks(s_a, s_b, *len_a, *len_b);
+	//print_stacks(s_a, s_b, *len_a, *len_b);
+	while(*len_a > 3)
+	{
 		i = 0;
 		while(i < *len_a)
 		{
-//			print_stacks(s_a, s_b, *len_a, *len_b);
-
 			tmp = strat_for_node(&cost, stacks, i);
 			int c = count_cost(tmp, cost);
 			if (count_cost(tmp, cost) < best.cost)
@@ -166,12 +266,14 @@ void	push_phase(int *s_a, int *s_b, int *len_a, int *len_b)
 				best.cost = count_cost(tmp, cost);
 				best.i = i;
 			}
-			printf("for %d cost is %d\n", i, c);
 			i++;
 		}
-		printf("===============================\n");
-		printf("best node: %d\nbest cost:%d\n", best.i, best.cost);
-//	}
+		tmp = strat_for_node(&cost, stacks, best.i);
+		apply_move(&stacks, &cost, &best, tmp);
+	//	print_stacks(s_a, s_b, *len_a, *len_b);
+
+	}
+	//print_stacks(s_a, s_b, *len_a, *len_b);
 
 
 
